d65af815a39be3ece36069d35406c92893f8388d
Aleksei <madd.niko@gmail.com>
2019-02-15 10:34:19 +0400

    (clear)

--- assets/js/common.js
+++ assets/js/common.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
                             },
                         }
                     } );
-                    console.log(result);
+
                 }
             }
         });
